48218f2790c5724bc5d2c96bd116d36d403dd63d
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-04 00:16:20 +0500

    Fix linter erros css

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function grab(e) {
-    return document.getElementById(e)
+  return document.getElementById(e);
 }
 
 const menu = document.getElementById('burger');
 const options = document.querySelectorAll('option');
 
 menu.addEventListener('click', () => {
-    grab('open').style.display = 'none'
-    grab('closed').style.display = 'block'
-    grab('nav--mobile').style.display = "block"
-})
+  grab('open').style.display = 'none';
+  grab('closed').style.display = 'block';
+  grab('nav--mobile').style.display = 'block';
+});
 
-grab('closed').addEventListener('click', ()=>{
-    grab('open').style.display = 'block'
-    grab('closed').style.display = 'none'
-    grab('nav--mobile').style.display = "none"
-})
+grab('closed').addEventListener('click', () => {
+  grab('open').style.display = 'block';
+  grab('closed').style.display = 'none';
+  grab('nav--mobile').style.display = 'none';
+});
