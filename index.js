21850adc3e2f532c7406d62209ba3f8987a19278
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-04 00:34:52 +0500

    Close mobile menu when pressing options

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ function grab(e) {
 }
 
 const menu = document.getElementById('burger');
-// const options = document.querySelectorAll('option');
+const options = document.querySelectorAll('.option');
 
 menu.addEventListener('click', () => {
   grab('open').style.display = 'none';
   grab('closed').style.display = 'block';
   grab('nav--mobile').style.display = 'block';
 });
+
+for (let i = 0; i < options.length; i += 1) {
+  options[i].addEventListener('click', () => {
+    grab('open').style.display = 'block';
+    grab('closed').style.display = 'none';
+    grab('nav--mobile').style.display = 'none';
+  });
+}
 
 grab('closed').addEventListener('click', () => {
   grab('open').style.display = 'block';
