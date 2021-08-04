cbb39187b7dee9890f3e5ed94675944ec8932b10
Busiwa24 <busiwaliuma@gmail.com>
2021-08-04 19:40:44 +0200

    Changes to javascript

--- index.js
+++ index.js
@@ -1,28 +1,108 @@
-function grab(e) {
-  return document.getElementById(e);
+// POPUP MENU
+
+const data = {
+  project1: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-1.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+
+  project2: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-2.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+
+  project3: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-3.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+  project4: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-4.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+  project5: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-5.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+  project6: {
+    name: 'Project Name Goes Here',
+    img: 'img/work-img-6.svg',
+    decription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tags: ['html', 'css', 'javaScript'],
+  },
+};
+
+// Popup menu variables////////////////////
+const nameID = document.getElementById('title');
+const img = document.getElementById('popup-img');
+const descrition = document.getElementById('description');
+const button1 = document.querySelector('#btn-1');
+const button2 = document.querySelector('#btn-2');
+const button3 = document.querySelector('#btn-3');
+const button4 = document.querySelector('#btn-4');
+const button5 = document.querySelector('#btn-5');
+const button6 = document.querySelector('#btn-6');
+const btnClosePop = document.querySelector('.popup-close-btn');
+const popup = document.getElementById('popup-menu');
+
+function openPopUp() {
+  popup.classList.remove('hidden');
 }
 
-const menu = document.getElementById('burger');
-const options = document.querySelectorAll('.option');
-
-menu.addEventListener('click', () => {
-  grab('open').style.display = 'none';
-  grab('closed').style.display = 'block';
-  grab('nav--mobile').style.display = 'block';
-});
-
-for (let i = 0; i < options.length; i += 1) {
-  options[i].addEventListener('click', () => {
-    grab('open').style.display = 'block';
-    grab('closed').style.display = 'none';
-    grab('nav--mobile').style.display = 'none';
-  });
+function changePopup(order) {
+  const project = data[`project${order}`];
+  nameID.innerHTML = project.name;
+  img.src = project.img;
+  descrition.innerHTML = project.decription;
+  openPopUp();
 }
 
-grab('closed').addEventListener('click', () => {
-  grab('open').style.display = 'block';
-  grab('closed').style.display = 'none';
+function closePopUp() {
+  popup.classList.add('hidden');
+}
 
-  grab('nav--mobile').style.display = 'none';
-  
+button1.addEventListener('click', () => {
+  openPopUp();
+  changePopup(1);
 });
+
+button2.addEventListener('click', () => {
+  openPopUp();
+  changePopup(2);
+});
+
+button3.addEventListener('click', () => {
+  openPopUp();
+  changePopup(3);
+});
+
+button4.addEventListener('click', () => {
+  openPopUp();
+  changePopup(4);
+});
+button5.addEventListener('click', () => {
+  openPopUp();
+  changePopup(4);
+});
+
+button6.addEventListener('click', () => {
+  openPopUp();
+  changePopup(4);
+});
+
+btnClosePop.addEventListener('click', closePopUp);
