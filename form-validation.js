3369d76ea1e7d8b3cb9bc5d0692ff4382ba82ae7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-06 18:29:43 +0500

    Finish validate form

--- form-validation.js
+++ form-validation.js
@@ -5,3 +5,26 @@ function validation() {
   const comment = form.elements.comment.value;
   const message = document.getElementById('message');
   message.innerHTML = '';
+
+  form.addEventListener('submit', (event) => {
+    if (name.length < 1 || name === undefined) {
+      event.preventDefault();
+      message.innerHTML = '* Make sure to fill your name';
+    } else if (
+      email !== email.toLowerCase() ||
+      email < 1 ||
+      email === undefined
+    ) {
+      event.preventDefault();
+      message.innerHTML =
+        '* Make sure you fill your email and that it is in lower case.';
+    } else if (comment.length < 5 || comment === undefined) {
+      event.preventDefault();
+      message.innerHTML = '* Text area should have at least 5 characters';
+    } else {
+      form.submit();
+    }
+  });
+}
+
+validation();
