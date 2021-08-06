ebbd2aa5bfb8b54ff8c1a93f08f931a36ed32720
Busiwa24 <busiwaliuma@gmail.com>
2021-08-06 15:38:43 +0200

    Linter error check

--- form-validation.js
+++ form-validation.js
@@ -12,8 +12,7 @@ function validation() {
       message.innerHTML = '* Make sure to fill your name';
     } else if (email !== email.toLowerCase() || email < 1 || email === undefined) {
       event.preventDefault();
-      message.innerHTML =
-        '* Make sure you fill your email and that it is in lower case.';
+      message.innerHTML = '* Make sure you fill your email and that it is in lower case.';
     } else if (comment.length < 5 || comment === undefined) {
       event.preventDefault();
       message.innerHTML = '* Text area should have at least 5 characters';
